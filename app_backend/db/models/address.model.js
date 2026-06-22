edac94c035713495f3745f956685a725efec54a8
Sohaib Nawaz <sohaib.nawaz@mmcgbl.com>
2026-06-22 17:12:04 +0500

    user model modify and address model created

---
+++ app_backend/db/models/address.model.js
@@ -0,0 +1,83 @@
+module.exports = (sequelize, DataTypes) => {
+    const addresses = sequelize.define(
+        "addresses",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+
+            user_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "users",
+                    key: "id",
+                },
+                onDelete: "CASCADE",
+                onUpdate: "CASCADE",
+            },
+
+            lat: {
+                type: DataTypes.DECIMAL(10, 8),
+                allowNull: true,
+            },
+
+            lon: {
+                type: DataTypes.DECIMAL(11, 8),
+                allowNull: true,
+            },
+
+            city: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+
+            state: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+
+            country: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+
+            zipcode: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+
+            addressline: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+
+            countrycode: {
+                type: DataTypes.STRING(10),
+                allowNull: true,
+            },
+
+            provincecode: {
+                type: DataTypes.STRING(10),
+                allowNull: true,
+            },
+        },
+        {
+            tableName: "addresses",
+            timestamps: true,
+            underscored: true,
+        }
+    );
+
+    // Associations
+    addresses.associate = (models) => {
+        addresses.belongsTo(models.users, {
+            foreignKey: "user_id",
+            as: "user",
+        });
+    };
+
+    return addresses;
+};
--- app_backend/db/models/users.js
+++ app_backend/db/models/users.js
@@ -1,68 +1,99 @@
 const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, DataTypes) => {
-    const users = sequelize.define('users', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        username: {
-            type: DataTypes.STRING(25),
-
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        role_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 2,
-            references: {
-                model: 'roles',
-                key: 'id'
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            default: false
-        },
-        email_verified: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        },
-        email_verification_token: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        email_verification_expires: {
-            type: DataTypes.DATE,
-            allowNull: true
-        }
-    },
+    const users = sequelize.define(
+        "users",
         {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+
+            username: {
+                type: DataTypes.STRING(25),
+                allowNull: false,
+            },
+
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+
+            role_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 2,
+                references: {
+                    model: "roles",
+                    key: "id",
+                },
+            },
+
+            email_verified: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+            },
+
+            email_verification_token: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+
+            email_verification_expires: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
+        },
+        {
+            tableName: "users",
+            timestamps: true,
+
             defaultScope: {
                 attributes: {
                     exclude: [
-                        "password"
+                        "password",
+                        "email_verification_token",
                     ],
+                },
+            },
+
+            scopes: {
+                withPassword: {
+                    attributes: {},
                 },
             },
         }
     );
 
+    // Associations
+    users.associate = (models) => {
+        users.hasMany(models.addresses, {
+            foreignKey: "user_id",
+            as: "addresses",
+        });
+
+        users.belongsTo(models.roles, {
+            foreignKey: "role_id",
+            as: "role",
+        });
+    };
+
+    // Hash password before create
     users.beforeCreate(async (user) => {
-        user.password = await bcrypt.hash(user.password, 10);
+        if (user.password) {
+            user.password = await bcrypt.hash(user.password, 10);
+        }
     });
 
+    // Hash password before update
     users.beforeUpdate(async (user) => {
         if (user.changed("password")) {
             user.password = await bcrypt.hash(user.password, 10);
@@ -70,4 +101,77 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     return users;
-}
+};
+
+// const bcrypt = require("bcrypt");
+
+// module.exports = (sequelize, DataTypes) => {
+//     const users = sequelize.define('users', {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             autoIncrement: true,
+//             primaryKey: true,
+//         },
+//         username: {
+//             type: DataTypes.STRING(25),
+
+//         },
+//         email: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//         },
+//         role_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             defaultValue: 2,
+//             references: {
+//                 model: 'roles',
+//                 key: 'id'
+//             }
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             default: false
+//         },
+//         email_verified: {
+//             type: DataTypes.BOOLEAN,
+//             allowNull: false,
+//             defaultValue: false
+//         },
+//         email_verification_token: {
+//             type: DataTypes.STRING,
+//             allowNull: true
+//         },
+//         email_verification_expires: {
+//             type: DataTypes.DATE,
+//             allowNull: true
+//         }
+//     },
+//         {
+//             defaultScope: {
+//                 attributes: {
+//                     exclude: [
+//                         "password"
+//                     ],
+//                 },
+//             },
+//         }
+//     );
+//     users.hasMany(models.addresses, {
+//         foreignKey: "user_id",
+//         as: "addresses",
+//     });
+//     users.beforeCreate(async (user) => {
+//         user.password = await bcrypt.hash(user.password, 10);
+//     });
+
+//     users.beforeUpdate(async (user) => {
+//         if (user.changed("password")) {
+//             user.password = await bcrypt.hash(user.password, 10);
+//         }
+//     });
+
+//     return users;
+// }
